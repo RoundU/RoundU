fc029494986a1cb7c8f6315c8c0ab4fcd3abb383
yeeeeeeeeee <marc-g-r@hotmail.com>
2019-04-23 18:16:54 +0200

    conexion bd

--- twitter_stream_download.py
+++ twitter_stream_download.py
@@ -7,6 +7,23 @@ import argparse
 import string
 import config
 import json
+import pyodbc 
+import datetime
+from time import gmtime, strftime
+
+separador = '¿¡@(,'
+directorioArchivo = '/home/marc/Escritorio/'
+queryParaTwitter = 'e'
+
+
+
+server = 'marcserver.database.windows.net' 
+database = 'RoundU_DB' 
+username = 'marc' 
+password = 'Barcelona2019' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password, autocommit=True)
+cursor = cnxn.cursor()
+
 
 barcelona = [2.090903, 41.335847, 2.220298, 41.461651] #Venid a verla, es una ciudad muy bonita!
 
@@ -31,17 +48,23 @@ class MyListener(StreamListener):
 
     def __init__(self, data_dir, query):
         query_fname = format_filename(query)
-        self.outfile = "%s/stream_%s.json" % (data_dir, query_fname)
+        self.outfile = "%s/stream_%s.csv" % (data_dir, query_fname)
 
     def on_data(self, data):
         try:
+            json_data = json.loads(data)
             with open(self.outfile, 'a') as f:
-                f.write(data.extended_tweet["full_text"])
-                return True
+                #Para meterlo en la base de datos
+                stringTextoParaSql = json_data['text'].replace("\'", "\"").replace("\n", " ")
+                textoEnSql = "INSERT INTO Mensaje (Texto, Usuario, Enlace) VALUES (REPLACE(REPLACE(REPLACE('"+stringTextoParaSql+"', '!', ''), '#', ''), '$', ''), " + str(json_data['user']['id']) +", NULL)"
+                cursor.execute(textoEnSql)
 
-        except AttributeError:
-            with open(self.outfile, 'a') as f:            
-                f.write(data)
+                print(json_data['text'])
+
+                #Para guardarlo en .csv
+                string_para_guardar = '\n' + stringTextoParaSql + separador + str(json_data['user']['id']) + separador + str(strftime("%Y-%m-%d %H:%M:%S", gmtime()))#datetime.datetime.now()
+                f.write(string_para_guardar)
+
                 return True
 
         except BaseException as e:
@@ -86,9 +109,16 @@ def parse(cls, api, raw):
 if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
+    args.data_dir = directorioArchivo
+    args.query = queryParaTwitter
+    print('args', args)
     auth = OAuthHandler(config.consumer_key, config.consumer_secret)
     auth.set_access_token(config.access_token, config.access_secret)
     api = tweepy.API(auth)
+
+#Para crear el fichero
+    with open("%s/stream_%s.csv" % (args.data_dir, format_filename(args.query)), 'a') as f:
+        f.write('Texto'+ separador+'IdUsuario'+ separador +'FechaCreacion')
 
     twitter_stream = Stream(auth, MyListener(args.data_dir, args.query))
 #twitter_stream.filter(locations=barcelona)
