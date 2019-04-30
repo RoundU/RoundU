3968014f23eb2236e93a745c05d608d15c9026fa
aitorAG <41166396+aitorAG@users.noreply.github.com>
2019-04-30 15:44:55 +0200

    Twitter stream to sql completed!

--- twitter_stream_to_sql.py
+++ twitter_stream_to_sql.py
@@ -31,7 +31,7 @@ def data_extract(json_):
     """
     msg_id = json_["id"]
     msg_users_id = json_["user"]["id"]
-    msg_timestamp = json_["created_at"]
+    msg_timestamp = json_["timestamp_ms"]
     if "RT @" in json_["text"]:
         if json_["retweeted_status"]["truncated"]:
             msg_text = json_["retweeted_status"]["extended_tweet"]["full_text"]
@@ -50,8 +50,8 @@ def data_extract(json_):
     users_followers = json_["user"]["followers_count"]
     user_tweets = json_["user"]["statuses_count"]
 
-    return (str(msg_id), str(msg_users_id), msg_timestamp, msg_text, 
-    users_name, users_id, users_desc, users_follows, users_followers, user_tweets)
+    return (str(msg_id), str(msg_users_id), str(msg_timestamp), msg_text, 
+    users_name, str(users_id), str(users_desc), users_follows, users_followers, user_tweets)
 
 
 
@@ -63,19 +63,17 @@ class MyListener(StreamListener):
     def on_status(self, data):
         
         try:            
-            js = json.dumps(data._json)
-            print(js["id"])
-            json_ = data_extract(json.dumps(data._json))
+            print(data_extract(data._json))
             
-            txt_SQL = data_extract(json_)
+            txt_SQL = data_extract(data._json)
                 
 #            stringTextoParaSql = json['text'].replace("\'", "\"").replace("\n", " ")
 #            textoEnSql = "INSERT INTO Mensaje (Texto, Usuario, Enlace) VALUES (REPLACE(REPLACE(REPLACE('"+stringTextoParaSql+"', '!', ''), '#', ''), '$', ''), " + str(json['user']['id']) +", NULL)"
-            cursor.execute('INSERT INTO dbo.msg VALUES ({}, {}, {}, N {}'.format(txt_SQL[0], txt_SQL[1], txt_SQL[2], txt_SQL[3]))
-            cursor.execute('INSERT INTO dbo.users VALUES (N {}, N {}, N {}, {}, {}, {}'.format(txt_SQL[4], txt_SQL[5], txt_SQL[6], txt_SQL[7],txt_SQL[8], txt_SQL[9] ))
+            cursor.execute('INSERT INTO dbo.msg VALUES ({}, {}, N "{}", N "{}"'.format(txt_SQL[0], txt_SQL[1], txt_SQL[2], txt_SQL[3]))
+            cursor.execute('INSERT INTO dbo.users VALUES (N "{}", N "{}", N "{}", {}, {}, {}'.format(txt_SQL[4], txt_SQL[5], txt_SQL[6], txt_SQL[7],txt_SQL[8], txt_SQL[9] ))
 
             print(txt_SQL[3])
-
+            print()
             return True
 
         except Exception as e:
@@ -97,6 +95,6 @@ if __name__ == '__main__':
     twitter_stream = Stream(auth, MyListener())
     twitter_stream.filter(
         locations = configLocal.coordenadas, 
-        track= [queryParaTwitter], 
+#        track= [queryParaTwitter], 
         languages=['es']
     )
